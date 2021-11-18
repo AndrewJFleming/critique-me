29d5a501b9728ab9681d1ea6a417d8116df1917b
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-11-17 17:09:11 -0800

    Remove Navbar Avatar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -65,13 +65,6 @@ const NavBar = () => {
       <Toolbar className={classes.toolbar}>
         {user ? (
           <div className={classes.profile}>
-            <Avatar
-              className={classes.green}
-              alt={user.result.name}
-              src={user.result.imageUrl}
-            >
-              {user.result.name.charAt(0)}
-            </Avatar>
             <Typography className={classes.userName} variant="h6">
               {user.result.name}
             </Typography>
